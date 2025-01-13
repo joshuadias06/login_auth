4ae8a42d69324fa7565b9135ee5f6e3d01fe1598
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-13 11:02:09 -0300

    Created AuthController

---
+++ src/main/java/com/login/desafio/klaston/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.login.desafio.klaston.controllers;
+
+import com.login.desafio.klaston.services.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+}
