f6e3bf593af0d605f1eb0a4057dca21a378fdad9
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-13 12:06:43 -0300

    Updated User Entity

--- src/main/java/com/login/desafio/klaston/models/User.java
+++ src/main/java/com/login/desafio/klaston/models/User.java
@@ -4,18 +4,43 @@ import jakarta.persistence.*;
 import lombok.*;
 
 
-@Getter
-@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "app_user")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String username;
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
