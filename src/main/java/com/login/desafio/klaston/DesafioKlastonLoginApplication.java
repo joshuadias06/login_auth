2694344eca59e2db1d3ed63351b094f1a48353da
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-13 10:24:20 -0300

    Practicing authentication, spring security and JWT

---
+++ src/main/java/com/login/desafio/klaston/DesafioKlastonLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.desafio.klaston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKlastonLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKlastonLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/desafio/klaston/DesafioKlastonLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.desafio.klaston;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioKlastonLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
