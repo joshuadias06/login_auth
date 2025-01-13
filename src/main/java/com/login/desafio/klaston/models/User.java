7a2a133d2a933e97f19b509e469b925040fc4b96
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-13 10:37:57 -0300

    Created Entity User

---
+++ src/main/java/com/login/desafio/klaston/models/User.java
@@ -0,0 +1,21 @@
+package com.login.desafio.klaston.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String password;
+}
