461a0960f05a3a5b6b2819b3fa4d7bac1a4497fc
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-14 16:47:56 -0300

    Updated script.js at login

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,30 +4,6 @@ function displayMessage(message, color) {
     messageDiv.textContent = message;
     messageDiv.style.color = color;
 }
-
-// Formulário de registro
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('regUsername').value;
-    const password = document.getElementById('regPassword').value;
-
-    fetch('http://localhost:8080/auth/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-    })
-        .then(response => response.text())
-        .then(message => {
-            displayMessage(message, 'green');
-        })
-        .catch(error => {
-            displayMessage('Erro ao registrar usuário!', 'red');
-        });
-});
-
 // Formulário de login
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
