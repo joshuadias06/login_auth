759abba397cacca5a20cb970df5a1f491a1326f0
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-16 12:06:14 -0300

    Fix Bugs WebConfig

--- src/main/java/com/login/desafio/klaston/config/WebConfig.java
+++ src/main/java/com/login/desafio/klaston/config/WebConfig.java
@@ -1,5 +1,7 @@
+
 package com.login.desafio.klaston.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,15 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63342")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedMethods("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:63342")  // Permite apenas o domínio especificado
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+                        .allowCredentials(true);  // Permite envio de credenciais (cookies, etc.)
             }
         };
     }
