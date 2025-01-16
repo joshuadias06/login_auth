686976fca74638c0ff10d6e48155eb091f811bc3
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-16 11:20:35 -0300

    Created CustomUserDetailService

---
+++ src/main/java/com/login/desafio/klaston/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.login.desafio.klaston.security;
+
+import com.login.desafio.klaston.models.User;  // Sua classe de modelo User
+import com.login.desafio.klaston.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // Injeção de dependência do UserRepository
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Busca o usuário no repositório usando o nome de usuário
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
