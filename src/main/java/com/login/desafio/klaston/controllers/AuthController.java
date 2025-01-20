86676aae7b0c48bf40687f03d8d8b17582d7acc5
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-20 19:25:05 -0300

    Created delete Method

--- src/main/java/com/login/desafio/klaston/controllers/AuthController.java
+++ src/main/java/com/login/desafio/klaston/controllers/AuthController.java
@@ -24,4 +24,10 @@ public class AuthController {
     public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
         return ResponseEntity.ok(userService.update(id, user));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
