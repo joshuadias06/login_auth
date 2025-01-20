8cd09cc5c41c180463b8513b30c267a8c911be36
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-20 19:27:30 -0300

    Recreated UserService class

--- src/main/java/com/login/desafio/klaston/services/UserService.java
+++ src/main/java/com/login/desafio/klaston/services/UserService.java
@@ -2,6 +2,7 @@ package com.login.desafio.klaston.services;
 
 import com.login.desafio.klaston.models.User;
 import com.login.desafio.klaston.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -16,37 +17,7 @@ public class UserService {
     * Function -> logoutUser
     * */
 
-    private final UserRepository userRepository;
-    private final Map<String, User> sessionMap = new HashMap<>();
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public String registerUser(User user){
-        if(userRepository.findByUsername(user.getUsername()).isPresent()){
-            return "User already exists";
-        }
-        userRepository.save(user);
-        return "User created";
-    }
-
-    public String loginUser(String username, String password){
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado com o nome de usuário fornecido."));
-        if(user.getPassword().equals(password)){
-            String sessionId = "SESSION-" + user.getId();
-            sessionMap.put(sessionId, user);
-            return sessionId;
-        }
-        return null; //Login Inválido!
-    }
-
-    public boolean validateSession(String sessionId){
-        return sessionMap.containsKey(sessionId);
-    }
-
-    public void logoutUser(String sessionId){
-        sessionMap.remove(sessionId);
-    }
+    private final Map<Long, User> userStore = new HashMap<>();
+    private final PasswordEncoder passwordEncoder;
+    
 }
