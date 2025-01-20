80e96125a59af16dc6d5c8efccf431e860ee842b
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-20 19:37:36 -0300

    Refactor JwtUtil

--- src/main/java/com/login/desafio/klaston/security/JwtUtil.java
+++ src/main/java/com/login/desafio/klaston/security/JwtUtil.java
@@ -8,43 +8,19 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
-public class JwtUtil {
+class JwtUtil {
 
-    private final String SECRET_KEY = "seu_segredo_aqui";
-
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }
+    private String SECRET_KEY = "secret";
 
     public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);  // Correção aqui
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
     }
 
-    public boolean isTokenValid(String token, String username) {
-        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
-    }
-
-    private <T> T extractClaim(String token, ClaimsResolver<T> claimsResolver) {  // Correção aqui
-        Claims claims = extractAllClaims(token);
-        return claimsResolver.resolve(claims);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractAllClaims(token).getExpiration().before(new Date());
-    }
-
-    // Interface funcional para o resolver da claim
-    @FunctionalInterface
-    interface ClaimsResolver<T> {
-        T resolve(Claims claims);
+    public String generateToken(String username) {
+        return Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 }
+
